0e572a6a7ed754e144d7667ff3ae82f3fe6150f2
eraderna <andrew.holmes@mastodonc.com>
2025-08-20 14:34:26 +0100

    Rename and improve default handling
    - Dropped 2022 from name as code is not 2022-based SNPPs specific, just that 2022 based SNPPs are default.
    - Parameterised defaults.

--- src/witan/population/england.clj
+++ src/witan/population/england.clj
@@ -1,11 +1,19 @@
-(ns witan.population.england.2022-based
-  "Functions to combine ONS mid-year population estimates with 2022 based 
-   subnational population projections to give a population dataset spanning 
-   the recent past and near future."
+(ns witan.population.england
+  "Functions to combine ONS mid-year population estimates with subnational 
+   population projections for English LA Districts or County/Unitary Authorities
+   to give a population dataset spanning the recent past and near future."
   (:require [tablecloth.api :as tc]
             [tablecloth.column.api :as tcc]
             [witan.population.england-wales.mye :as mye]
             [witan.population.england.snpp-2022 :as snpp-2022]))
+
+(def default-by-lad-options
+  {::mye-f  mye/->dataset-by-lad
+   ::snpp-f snpp-2022/->dataset-by-lad})
+
+(def default-by-ctyua-options
+  {::mye-f  mye/->dataset-by-ctyua
+   ::snpp-f snpp-2022/->dataset-by-ctyua})
 
 (defn concat-mye-snpp
   "Concatenates MYE estiamtes from `mye-ds` with SNPPs from `snpp-ds`.
@@ -30,11 +38,11 @@
                                      (tc/dataset-name snpp-ds)
                                      min-snpp-year)))))
 
-(comment ;; Example: LAD level concatenation of MYE & SNPP for Surrey 10 year olds from 2021 to 2025 with 2023 as min-snpp-year
+(comment ;; Example: LAD level concatenation of default MYE & 2022 based SNPPs for Surrey 10 year olds from 2020 to 2025 with 2023 as min-snpp-year
   (let [options {:ladcd-f       #{"E06000005"}
                  :min-age       10
                  :max-age       10
-                 :min-year      2021
+                 :min-year      2020
                  :min-snpp-year 2023
                  :max-year      2025}
         mye-ds  (-> options
@@ -48,10 +56,11 @@
      :concat-ds (concat-mye-snpp mye-ds
                                  snpp-ds
                                  options)})
-  ;;=> {:mye-ds [myebtablesenglandwales/myebtablesenglandwales20112024.xlsx]MYEB1: long by LAD (persons) [4 9]:
+  ;;=> {:mye-ds [myebtablesenglandwales/myebtablesenglandwales20112024.xlsx]MYEB1: long by LAD (persons) [5 9]:
   ;;   
   ;;   |  :lad23cd |   :lad23nm | :ctyua23cd | :ctyua23nm | :country | :year | :age |    :sex | :population |
   ;;   |-----------|------------|------------|------------|----------|------:|-----:|---------|------------:|
+  ;;   | E06000005 | Darlington |  E06000005 | Darlington |        E |  2020 |   10 | persons |      1323.0 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |        E |  2021 |   10 | persons |      1320.0 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |        E |  2022 |   10 | persons |      1353.0 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |        E |  2023 |   10 | persons |      1347.0 |
@@ -66,17 +75,18 @@
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2025 |         10 |   10 | Population | persons |    1253.260 |
   ;;   ,
   ;;    :concat-ds
-  ;;    [myebtablesenglandwales/myebtablesenglandwales20112024.xlsx]MYEB1: long by LAD (persons) and 2022snpppopulationsyoa/2022snpppopulationsyoamigcat23/2022 SNPP Population persons.csv: long by LAD concatenated at min-snpp-year of 2023 [5 8]:
+  ;;    [myebtablesenglandwales/myebtablesenglandwales20112024.xlsx]MYEB1: long by LAD (persons) and 2022snpppopulationsyoa/2022snpppopulationsyoamigcat23/2022 SNPP Population persons.csv: long by LAD concatenated at min-snpp-year of 2023 [6 8]:
   ;;   
   ;;   |  :lad23cd |   :lad23nm | :ctyua23cd | :ctyua23nm | :year | :age |    :sex | :population |
   ;;   |-----------|------------|------------|------------|------:|-----:|---------|------------:|
+  ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2020 |   10 | persons |    1323.000 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2021 |   10 | persons |    1320.000 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2022 |   10 | persons |    1353.000 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2023 |   10 | persons |    1314.757 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2024 |   10 | persons |    1276.514 |
   ;;   | E06000005 | Darlington |  E06000005 | Darlington |  2025 |   10 | persons |    1253.260 |
   ;;   }
-   
+
   :rcf)
 
 (defn ->dataset
@@ -84,10 +94,10 @@
    at minimum `:year` of SNPPs unless value for `:min-snpp-year` option is specified.
    MYEs & SNPPs are obtained via functions specified by `::mye-f` & `::snpp-f` 
    option values (if min/switch/max year require them), and are passed the options map."
-  [& {::keys [snpp-f mye-f]
+  [& {::keys [mye-f snpp-f]
       :keys [min-year min-snpp-year max-year]
-      :or {snpp-f snpp-2022/->dataset-by-ctyua
-           mye-f  mye/->dataset-by-ctyua}
+      :or {mye-f  (default-by-ctyua-options ::mye-f)
+           snpp-f (default-by-ctyua-options ::snpp-f)}
       :as options}]
   (let [snpp-ds       (when (or (nil? min-snpp-year)
                                 (nil? max-year)
@@ -107,7 +117,7 @@
                      snpp-ds
                      (assoc options :min-snpp-year min-snpp-year))))
 
-(comment ;; Example: CTYUA level for Surrey 10 year olds from 2020 to 2025 with default min-snpp-year
+(comment ;; Example: CTYUA level for Surrey 10 year olds from 2020 to 2025 from default MYE & 2022 based SNPPs with default min-snpp-year
   (-> {:ctyuanm-f #{"Surrey"}
        :min-age   10
        :max-age   10
@@ -125,22 +135,20 @@
   ;;   |  E10000030 |     Surrey |  2024 |   10 | persons |   15173.479 |
   ;;   |  E10000030 |     Surrey |  2025 |   10 | persons |   15211.734 |
   ;;   
-  
+
   :rcf)
 
 (defn ->dataset-by-ctyua
   [& {:as options}]
-  (->dataset (merge {::snpp-f snpp-2022/->dataset-by-ctyua
-                     ::mye-f  mye/->dataset-by-ctyua}
+  (->dataset (merge default-by-ctyua-options
                     options)))
 
 (defn ->dataset-by-lad
   [& {:as options}]
-  (->dataset (merge {::snpp-f snpp-2022/->dataset-by-lad
-                     ::mye-f  mye/->dataset-by-lad}
+  (->dataset (merge default-by-lad-options
                     options)))
 
-(comment ;; Example: LAD level for Surrey 10 year olds from 2020 to 2025 with default min-snpp-year
+(comment ;; Example: LAD level for Surrey 10 year olds from 2020 to 2025 from default MYE & 2022 based SNPPs with default min-snpp-year
   (-> {:ctyuanm-f #{"Surrey"}
        :min-age   10
        :max-age   10
@@ -174,5 +182,5 @@
   ;;   | E07000217 |          Woking |  E10000030 |     Surrey |  2024 |   10 | persons |    1287.245 |
   ;;   | E07000217 |          Woking |  E10000030 |     Surrey |  2025 |   10 | persons |    1299.864 |
   ;;   
-  
+
   :rcf)
